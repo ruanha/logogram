d48f012395065abd9427f8ef72c5ecefadbc908a
Rune Hartvig <rune_hartvig@hotmail.com>
2023-02-05 21:40:09 +0100

    style output as an arm with thumb

--- src/components/Output.tsx
+++ src/components/Output.tsx
@@ -1,8 +1,17 @@
 export default function Output({ value }: { value: string }) {
   return (
     <div className="output-box">
-      <div className="output">
+      <div className="arm">
         <div className="output-text">约翰</div>
+      </div>
+
+      <div className="palm"></div>
+
+      <div className="fingers">
+        <div className="finger"></div>
+        <div className="finger"></div>
+        <div className="finger"></div>
+        <div className="finger"></div>
       </div>
     </div>
   );
