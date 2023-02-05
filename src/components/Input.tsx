a393c05a16f82952bdb48d54667a618d54b5a7e2
Rune Hartvig <rune_hartvig@hotmail.com>
2023-02-05 20:26:21 +0100

    style input and make it a form + label

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,10 +1,15 @@
 export default function Input({ value, onChange }: InputProps) {
   return (
-    <input
-      type="text"
-      value={value}
-      onChange={(e) => onChange(e.target.value)}
-    />
+    <form>
+      <label htmlFor="first-name">Input your first name:</label>
+      <input
+        required
+        id="first-name"
+        type="text"
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+      />
+    </form>
   );
 }
 
